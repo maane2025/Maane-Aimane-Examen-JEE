625ede43dabece6e5af98ead078977368ea3f99d
maane2025 <maane2025@users.noreply.github.com>
2025-05-19 09:03:40 +0100

    Initial commit

---
+++ src/test/java/com/maane/examjee/MaaneAimaneExamenJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maane.examjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaaneAimaneExamenJeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
